76b51bee2b7562b1879ffde61365a4d61cadfe82
Mario Loriedo <mario.loriedo@gmail.com>
2017-11-22 02:01:13 +0100

    Updated sev labels names, added URLs to list of issues

--- src/main/java/com/github/l0rd/stats4osio/OsioStatsGenerator.java
+++ src/main/java/com/github/l0rd/stats4osio/OsioStatsGenerator.java
@@ -24,8 +24,10 @@
 package com.github.l0rd.stats4osio;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.List;
+import org.apache.http.client.utils.URIBuilder;
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.Label;
 import org.eclipse.egit.github.core.client.GitHubClient;
@@ -36,8 +38,8 @@ import org.slf4j.LoggerFactory;
 
 class OsioStatsGenerator {
 
-  private static final String SEVERITY_BLOCKER = "severity/blocker";
-  private static final String SEVERITY_P1 = "severity/P1";
+  private static final String SEV1_URGENT = "SEV1-urgent";
+  private static final String SEV2_HIGH = "SEV2-high";
 
   public static void main(String[] args) throws IOException {
 
@@ -49,11 +51,17 @@ class OsioStatsGenerator {
     IssueService issueService = new IssueService();
     LabelService labelService = new LabelService();
     GitHubClient client = new GitHubClient();
-    if (args.length > 0) {
+    if (args.length == 1) {
       String token = args[0];
       client.setOAuth2Token(token);
       issueService.getClient().setOAuth2Token(token);
       labelService.getClient().setOAuth2Token(token);
+    } else if (args.length == 2) {
+      String user = args[0];
+      String password = args[1];
+      client.setCredentials(user, password);
+      issueService.getClient().setCredentials(user, password);
+      labelService.getClient().setCredentials(user, password);
     } else {
       logger.warn(
           "We are going to use GitHub API without authentication and the rate limite is very low. "
@@ -69,28 +77,42 @@ class OsioStatsGenerator {
 
     for (Label teamLabel : labels) {
       String team = teamLabel.getName();
-      List<Issue> blockers = getBlockers(gitHubOrg, gitHubRepo, issueService, team);
-      List<Issue> p1 = getP1(gitHubOrg, gitHubRepo, issueService, team);
-      System.out.println(team + "," + blockers.size() + "," + p1.size());
+      List<Issue> sev1 = getSev1(gitHubOrg, gitHubRepo, issueService, team);
+      String sev1URL = getFilteredIssuesURL(gitHubOrg, gitHubRepo, team, SEV1_URGENT);
+      List<Issue> sev2 = getSev2(gitHubOrg, gitHubRepo, issueService, team);
+      String sev2URL = getFilteredIssuesURL(gitHubOrg, gitHubRepo, team, SEV2_HIGH);
+      System.out.println(
+          team + " ," + sev1.size() + " ," + sev1URL + " ," + sev2.size() + " ," + sev2URL);
     }
   }
 
   private static void printCSVHeader() {
-    System.out.println("team, blockers, P1");
+    System.out.println("team, SEV1-COUNT, SEV1-URL, SEV2-COUNT, SEV2-URL");
   }
 
-  private static List<Issue> getBlockers(String gitHubOrg, String gitHubRepo,
+  private static List<Issue> getSev1(String gitHubOrg, String gitHubRepo,
       IssueService issueService, String team) throws IOException {
     HashMap<String, String> blockerFilter = new HashMap<String, String>() {{
-      put(IssueService.FILTER_LABELS, SEVERITY_BLOCKER + "," + team);
+      put(IssueService.FILTER_LABELS, SEV1_URGENT + "," + team);
     }};
     return issueService.getIssues(gitHubOrg, gitHubRepo, blockerFilter);
   }
 
-  private static List<Issue> getP1(String gitHubOrg, String gitHubRepo,
+  private static String getFilteredIssuesURL(String gitHubOrg, String gitHubRepo, String team,
+      String sev) {
+    try {
+      return new URIBuilder("https://github.com/" + gitHubOrg + "/" + gitHubRepo + "/issues")
+          .addParameter("q", "is:open is:issue label:" + team + " label:" + sev).build().toString();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    }
+    return "";
+  }
+
+  private static List<Issue> getSev2(String gitHubOrg, String gitHubRepo,
       IssueService issueService, String team) throws IOException {
     HashMap<String, String> blockerFilter = new HashMap<String, String>() {{
-      put(IssueService.FILTER_LABELS, SEVERITY_P1 + "," + team);
+      put(IssueService.FILTER_LABELS, SEV2_HIGH + "," + team);
     }};
     return issueService.getIssues(gitHubOrg, gitHubRepo, blockerFilter);
   }
